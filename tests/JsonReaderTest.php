d71aa6683a205209b76241e91f46c1d685ef9e33
Justin Hendrickson <justin.hendrickson@nerdery.com>
2015-09-28 11:39:07 -0500

    Refactoring to use latest version of stream reader. Refactored to use the same interface as ext-jsonreader (https://github.com/shevron/ext-jsonreader)

--- tests/JsonReaderTest.php
+++ tests/JsonReaderTest.php
@@ -2,14 +2,12 @@
 namespace Toxygene\JsonReader\Tests;
 
 use Toxygene\JsonReader\JsonReader;
-use Toxygene\StreamReader\PeekableStreamReaderDecorator;
-use Toxygene\StreamReader\StreamReader;
 
 class JsonReaderTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
-     * @var
+     * @var resource
      */
     private $stream;
 
@@ -21,7 +19,9 @@ class JsonReaderTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->stream = fopen('php://memory', 'w+');
-        $this->reader = new JsonReader(new PeekableStreamReaderDecorator(new StreamReader($this->stream)));
+
+        $this->reader = new JsonReader();
+        $this->reader->openStream($this->stream);
     }
 
     public function tearDown()
@@ -37,12 +37,36 @@ class JsonReaderTest extends \PHPUnit_Framework_TestCase
         $this->assertReadNode(JsonReader::STRING, 'value');
     }
 
+    private function setStreamContents($contents)
+    {
+        fseek($this->stream, 0);
+        fwrite($this->stream, $contents);
+        fseek($this->stream, 0);
+
+        return $this;
+    }
+
+    private function assertReadNode($type, $value = null)
+    {
+        $this->assertTrue($this->reader->read());
+
+        $this->assertEquals(
+            $type,
+            $this->reader->tokenType
+        );
+
+        $this->assertEquals(
+            $value,
+            $this->reader->value
+        );
+    }
+
     public function testAnObjectCanBeRead()
     {
         $this->setStreamContents('{"key": "value"}');
 
         $this->assertReadNode(JsonReader::OBJECT_START);
-        $this->assertReadNode(JsonReader::MEMBER_NAME, 'key');
+        $this->assertReadNode(JsonReader::OBJECT_KEY, 'key');
         $this->assertReadNode(JsonReader::STRING, 'value');
         $this->assertReadNode(JsonReader::OBJECT_END);
     }
@@ -61,42 +85,18 @@ class JsonReaderTest extends \PHPUnit_Framework_TestCase
         $this->setStreamContents('{"one":[{"two": "three"},{"four": "five"}]}');
 
         $this->assertReadNode(JsonReader::OBJECT_START);
-        $this->assertReadNode(JsonReader::MEMBER_NAME, 'one');
+        $this->assertReadNode(JsonReader::OBJECT_KEY, 'one');
         $this->assertReadNode(JsonReader::ARRAY_START);
         $this->assertReadNode(JsonReader::OBJECT_START);
-        $this->assertReadNode(JsonReader::MEMBER_NAME, 'two');
+        $this->assertReadNode(JsonReader::OBJECT_KEY, 'two');
         $this->assertReadNode(JsonReader::STRING, 'three');
         $this->assertReadNode(JsonReader::OBJECT_END);
         $this->assertReadNode(JsonReader::OBJECT_START);
-        $this->assertReadNode(JsonReader::MEMBER_NAME, 'four');
+        $this->assertReadNode(JsonReader::OBJECT_KEY, 'four');
         $this->assertReadNode(JsonReader::STRING, 'five');
         $this->assertReadNode(JsonReader::OBJECT_END);
         $this->assertReadNode(JsonReader::ARRAY_END);
         $this->assertReadNode(JsonReader::OBJECT_END);
-    }
-
-    private function assertReadNode($type, $value = null)
-    {
-        $this->assertTrue($this->reader->read());
-
-        $this->assertEquals(
-            $type,
-            $this->reader->nodeType
-        );
-
-        $this->assertEquals(
-            $value,
-            $this->reader->nodeValue
-        );
-    }
-
-    private function setStreamContents($contents)
-    {
-        fseek($this->stream, 0);
-        fwrite($this->stream, $contents);
-        fseek($this->stream, 0);
-
-        return $this;
     }
 
 }
